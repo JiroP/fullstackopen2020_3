258466010dc2ec4e61472934cf8fb18414116a03
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-09-13 17:43:33 +0300

    build setup frontend

---
+++ build/precache-manifest.e30a0e1e1d4c8b290c5926ed4df31769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69328ee58e19eb9acac3d030f2a38583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a73c1f86544e4c04678",
+    "url": "/static/css/main.667277bb.chunk.css"
+  },
+  {
+    "revision": "e073dc81b08e917e5e6d",
+    "url": "/static/js/2.5a58188a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a58188a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a73c1f86544e4c04678",
+    "url": "/static/js/main.28695af0.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
